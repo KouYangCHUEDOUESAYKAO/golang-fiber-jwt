9795ebbaafa876ed6f7c0f6693f78fb3c038e0b9
KouYangCHUEDOUESAYKAO <kouyanghlubkoj@gmail.com>
2023-07-28 11:57:31 +0700

    second commit

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 echo "# golang-fiber-jwt" >> README.md
 git init
 git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/KouYangCHUEDOUESAYKAO/golang-fiber-jwt.git
+git push -u origin master
+
+OR
+
+git remote add origin https://github.com/KouYangCHUEDOUESAYKAO/golang-fiber-jwt.git
+git branch -M master
+git push -u origin master
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
+	"github.com/KouYangCHUEDOUESAYKAO/golang-fiber-jwt/controllers"
 	"github.com/KouYangCHUEDOUESAYKAO/golang-fiber-jwt/initializers"
+	"github.com/KouYangCHUEDOUESAYKAO/golang-fiber-jwt/middleware"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
@@ -18,13 +22,37 @@ func init() {
 
 func main() {
 	app := fiber.New()
+	micro := fiber.New()
 
+	app.Mount("/api", micro)
 	app.Use(logger.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:4200",
+		AllowHeaders:     "Origin, Content-Type, Accept",
+		AllowMethods:     "GET, POST, PUT, DELETE",
+		AllowCredentials: true,
+	}))
+
+	micro.Route("/auth", func(router fiber.Router) {
+		router.Post("/register", controllers.SignUpUser)
+		router.Post("/login", controllers.SignInUser)
+		router.Post("/logout", middleware.DeserializeUser, controllers.LogoutUser)
+	})
+
+	micro.Get("/users/me", middleware.DeserializeUser, controllers.GetMe)
 
 	app.Get("/api/healthchecker", func(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusOK).JSON(fiber.Map{
 			"status":  "success",
 			"message": "Welcome to Golang, Fiber, and GORM",
+		})
+	})
+
+	micro.All("*", func(c *fiber.Ctx) error {
+		path := c.Path()
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"status":  "fail",
+			"message": fmt.Sprintf("Path: %v does not exists on this server", path),
 		})
 	})
 
